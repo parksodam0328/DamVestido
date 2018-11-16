ccd0e5cfcb414ade300a28dd22ec979e8848d0ce
Park So Dam <parksodam0228@e-mirim.hs.kr>
2018-11-16 13:42:10 +0900

    index 페이지 이미지 배치

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="utf8" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dam Vestido</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <style>
+    img{
+      margin-right: 20px;
+      margin-left: 20px;
+    }
+    </style>
+  </head>
+  <body>
+    <?php include("top.php");?>
+    <center>
+      <img src="images/best_seller.png" width="260px" style="margin-top:10px; margin-bottom:10px;"><br>
+
+      <img src="images/b1.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+      <img src="images/b2.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+      <img src="images/b3.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+      <img src="images/b4.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;"><br>
+
+      <img src="images/new_arrival.png" width="260px" style="margin-top:10px; margin-bottom:10px;"><br>
+
+      <img src="images/n1.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+      <img src="images/n2.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+      <img src="images/n3.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+      <img src="images/n4.png" width="260px" height="260px" style="margin-top:10px; margin-bottom:30px;">
+    </center>
+
+    <?php include("bottom.php");?>
+  </body>
+</html>
